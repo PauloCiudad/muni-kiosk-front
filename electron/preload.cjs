058d8dda3b2b38afdcdcb4dbed97e213276fdb73
pciudad <paulociudadq@gmail.com>
2026-02-19 17:26:15 -0500

    Ventana escrtorio

---
+++ electron/preload.cjs
@@ -0,0 +1,7 @@
+const { contextBridge } = require("electron");
+
+// Por ahora no exponemos nada.
+// (Tu app sigue igual, consumiendo APIs por fetch)
+contextBridge.exposeInMainWorld("desktop", {
+  ping: () => "pong",
+});
